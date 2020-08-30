088e023897fd29f21e4aad05db3e7028a48dd683
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-08-30 15:43:25 +0200

    style Tour Component

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
+import "./tour.scss";
 
 class Tour extends Component {
   render() {
-    const { city, img, name, info } = this.props.tour;
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
     return (
-      <div>
-        <h1>{city}</h1>
-        <img src={img} alt={city} />
-        <h3>{name}</h3>
-        <p>{info}</p>
-      </div>
+      <article className="tour">
+        <div className="img-container">
+          <img src={img} alt={name} />
+          <span className="close-btn">
+            <i className="fas fa-window-close" />
+          </span>
+        </div>
+        <div className="tour-info">
+          <h3>{city}</h3>
+          <h4>{name}</h4>
+          <h5>
+            info{" "}
+            <span>
+              <i className="fas fa-caret-square-down" />
+            </span>
+          </h5>
+          <p>{info}</p>
+        </div>
+      </article>
     );
   }
 }
